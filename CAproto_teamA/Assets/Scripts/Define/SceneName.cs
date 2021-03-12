480aa91db9c006126a09f973140b811676465aca
M-Yuki-Hasuo <huyuda1220@gmail.com>
2021-03-12 12:25:09 +0900

    add:defineクラスを作成し、SceneControllerをシングルトン化した

---
+++ CAproto_teamA/Assets/Scripts/Define/SceneName.cs
@@ -0,0 +1,12 @@
+﻿namespace SceneName
+{
+    public class SceneName
+    {
+        public const string ResultScene = "Result";
+        public const string InGameScene = "InGame";
+        public const string TitleScene = "Title";
+        public const string SignUpScene = "SignUp";
+        public const string LoginScene = "Login";
+        public const string RankingScene = "Ranking";
+    }
+}
---
+++ CAproto_teamA/Assets/Scripts/SceneController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using SceneName;
+
+public class SceneController : MonoBehaviour
+{
+    private static SceneController instance;
+
+    public static SceneController Instance
+    {
+
+        get
+        {
+            if (instance == null)
+            {
+
+                GameObject single = new GameObject();
+                //instanceに格納されてる値を管理する
+                instance = single.AddComponent<SceneController>();
+                //scene跨いでもインスタンスが残るのでnull処理に行かない
+                DontDestroyOnLoad(instance);
+
+            }
+            return instance;
+
+        }
+    }
+
+    public void LoadResultScene()
+    {
+        SceneManager.LoadScene(SceneName.SceneName.ResultScene);
+    }
+
+    public void LoadInGameScene()
+    {
+        SceneManager.LoadScene(SceneName.SceneName.InGameScene);
+    }
+
+    public void LoadRankingScene()
+    {
+        SceneManager.LoadScene(SceneName.SceneName.ResultScene);
+    }
+
+    public void LoadTitleScene()
+    {
+        SceneManager.LoadScene(SceneName.SceneName.TitleScene);
+    }
+
+    public void LoadLoginScene()
+    {
+        SceneManager.LoadScene(SceneName.SceneName.LoginScene);
+    }
+
+    public void LoadSignUpScene()
+    {
+        SceneManager.LoadScene(SceneName.SceneName.SignUpScene);
+    }
+}
