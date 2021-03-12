627ed88151ac8aba26191e4c1937e6f309000702
M-Yuki-Hasuo <huyuda1220@gmail.com>
2021-03-12 14:11:00 +0900

    add:TitleのUI追加及びGameManagerの実装

---
+++ CAproto_teamA/Assets/Scripts/Title/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Title
+{
+
+    public class GameManager : MonoBehaviour
+    {
+        [SerializeField] private Button howToPlay;
+        [SerializeField] private Button signUp;
+        [SerializeField] private Button login;
+
+
+
+        /// <summary>
+        /// Titleでは、GameManagerにUIの処理を書いてます。
+        /// </summary>
+        void Start()
+        {
+            howToPlay.onClick.AddListener(HowToPlay);
+            signUp.onClick.AddListener(LoadSignUp);
+            login.onClick.AddListener(LoadSignIn);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+
+        private void LoadSignUp()
+        {
+            SceneController.Instance.LoadSignUpScene();
+        }
+
+        private void LoadSignIn()
+	    {
+            SceneController.Instance.LoadLoginScene();
+        }
+
+        /// <summary>
+	    /// DOTweenでモーダルウィンドーっぽくするのがかっこよくないか？
+	    /// </summary>
+        private void HowToPlay()
+        {
+
+        }
+
+    }
+}
