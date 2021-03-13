22b2664f140f9eb2d626d44617dd4a5809a13c2e
M-Yuki-Hasuo <huyuda1220@gmail.com>
2021-03-13 20:45:57 +0900

    add: 動くステージ

---
+++ CAproto_teamA/Assets/Scripts/hoge/BoomController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoomController : MonoBehaviour
+{
+    private float boomSpeed = -100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveBoom();
+        ReturnPosition();
+    }
+
+
+    private void MoveBoom()
+	{
+        gameObject.transform.position += new Vector3(0, 0, boomSpeed);
+    }
+
+    /// <summary>
+	/// ごめん、後で絶対直すからハードコーディングさせて...
+	/// </summary>
+    private void ReturnPosition()
+    {
+		if (gameObject.transform.position.z < 1000)
+		{
+            Initialize();
+        }
+       
+
+    }
+
+	private void Initialize()
+	{
+        gameObject.transform.position = new Vector3(-874, -292.9f, 4967);
+    }
+
+}
---
+++ CAproto_teamA/Assets/Scripts/hoge/StageController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageController : MonoBehaviour
+{
+    private float stageSpeed = -100f;
+    [SerializeField] private Vector3 position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveBoom();
+        ReturnPosition();
+    }
+
+    private void MoveBoom()
+    {
+        gameObject.transform.position += new Vector3(0, 0, stageSpeed);
+    }
+
+    /// <summary>
+	/// ごめん、後で絶対直すからハードコーディングさせて...
+	/// </summary>
+    private void ReturnPosition()
+    {
+        if (gameObject.transform.position.z < -500)
+        {
+            Initialize();
+        }
+
+
+    }
+
+    private void Initialize()
+    {
+        gameObject.transform.position = new Vector3(position.x, position.y, position.z);
+    }
+}
