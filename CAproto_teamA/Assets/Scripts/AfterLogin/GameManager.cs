bc6274fe495b713260287291f3feb6246ee5ccd8
M-Yuki-Hasuo <huyuda1220@gmail.com>
2021-03-13 16:55:56 +0900

    add:UI画像を加えログイン後のSceneを追加した

---
+++ CAproto_teamA/Assets/Scripts/AfterLogin/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace Title_afterLogin
+{
+    public class GameManager : MonoBehaviour
+    {
+        [SerializeField] private Button button; 
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            button.onClick.AddListener(LoadInGameScene);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+
+        private void LoadInGameScene()
+		{
+            SceneController.Instance.LoadInGameScene();
+		}
+    }
+}
