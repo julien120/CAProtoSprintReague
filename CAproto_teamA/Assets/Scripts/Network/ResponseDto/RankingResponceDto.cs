0913ae8cb12b67dc9ace088d39847da76d52ca47
konie_52 <you524business@gmail.com>
2021-03-14 00:10:07 +0900

    ランキングメソッドの追加

---
+++ CAproto_teamA/Assets/Scripts/Network/ResponseDto/RankingResponceDto.cs
@@ -0,0 +1,19 @@
+//using System;
+
+//namespace ProjectConnect.Network.ResponseDto
+//{
+//	[Serializable]
+//	public class RankingResponseDto : DtoBase
+//	{
+//		string userId;
+//		string userName;
+//		string rank;
+//		string score;
+
+//		//public string[][] ranks = new string[10][] { userId, userName,rank,score};
+
+//		//ranks[1]={userId,
+//	}
+
+
+//}
