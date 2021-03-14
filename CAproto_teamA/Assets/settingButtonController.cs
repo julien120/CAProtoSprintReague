7a9625fa109c9cf0e7afc9e2f6c96bef0c199385
M-Yuki-Hasuo <huyuda1220@gmail.com>
2021-03-14 14:29:23 +0900

    fix Ranking and Setting

---
+++ CAproto_teamA/Assets/settingButtonController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace LoginedTitle
+{
+    public class settingButtonController : MonoBehaviour
+    {
+        [SerializeField] private GameObject setting;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void HideSetting()
+        {
+            setting.SetActive(false);
+        }
+    }
+
+
+}
+
