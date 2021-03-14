4297f6637e4a5cc8a28b1fe9953ca6aef639ee47
M-Yuki-Hasuo <huyuda1220@gmail.com>
2021-03-14 14:00:31 +0900

    add ランキングUIのprefab化、そのまま読み込んだら行ける

---
+++ CAproto_teamA/Assets/Scripts/Ranking/RankingButtonController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RankingButtonController : MonoBehaviour
+{
+    [SerializeField] private Button buttonOK;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonOK.onClick.AddListener(LoadLoginedTitle);
+    }
+
+    private void LoadLoginedTitle()
+	{
+        SceneController.Instance.LoadLoginedTitleScene();
+	}
+}
